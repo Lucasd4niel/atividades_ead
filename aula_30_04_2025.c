9c8cd386329e81f3bc0c295045c201b5be4d463a
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-04-30 10:00:07 -0400

    v2

--- aula_30_04_2025.c
+++ aula_30_04_2025.c
@@ -4,25 +4,28 @@ int main(void)
 {
     int a, b, a_simp, b_simp, maior, i;
     scanf("%d/%d", &a, &b);
-    i = 1;
-    if(a >= b)
+    if (b != 0)
     {
-        maior = a;
-    }
-    else
-    {
-        maior = b;
-    }
-    i = 1;
-    do
-    {
-        if(a % i == 0 && b % i == 0)
+        i = 1;
+        if(a >= b)
         {
-            a_simp = a / i;
-            b_simp = b / i;
+            maior = a;
         }
-        i++;
-    }while (i <= maior);
-    printf("%d/%d", a_simp, b_simp);
+        else
+        {
+            maior = b;
+        }
+        i = 1;
+        do
+        {
+            if(a % i == 0 && b % i == 0)
+            {
+                a_simp = a / i;
+                b_simp = b / i;
+            }
+            i++;
+        }while (i <= maior);
+        printf("%d/%d", a_simp, b_simp);
+    }
     return 0;
 }
