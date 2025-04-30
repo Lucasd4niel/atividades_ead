665eea7e49b71068d82b2d917022c471388d2007
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-04-30 09:36:38 -0400

    atividade 2 feita

---
+++ aula_30_04_2025.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, a_simp, b_simp, maior, i;
+    scanf("%d/%d", &a, &b);
+    if(a >= b)
+    {
+        maior = a;
+    }
+    else
+    {
+        maior = b;
+    }
+    i = 1;
+    do
+    {
+        if(a % i == 0 && b % i == 0)
+        {
+            a_simp = a / i;
+            b_simp = b / i;
+        }
+        i++;
+    }while (i <= maior);
+    printf("%d/%d", a_simp, b_simp);
+    return 0;
+}
